70c8047cd5dbee9089fcfbb4e036534b3545a44b
Musfiqur Sakib <m.r.sakib@outlook.com>
2025-05-15 00:02:14 +1000

    implement sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,15 @@
+import Sidebar from '@/components/Sidebar';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Sakib', lastName: 'M' }; // Simulating a logged-in user
+
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
       {children}
     </main>
   );
