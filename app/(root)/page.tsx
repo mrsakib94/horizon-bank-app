ff163a364c3e3080211e0c3b2fecdc3853ba064b
Musfiqur Sakib <m.r.sakib@outlook.com>
2025-05-15 13:32:29 +1000

    implement right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-  const loggedIn = { firstName: 'Sakib' };
+  const loggedIn = {
+    firstName: 'Sakib',
+    lastName: 'M',
+    email: 'sakib.m@test.com',
+  };
 
   return (
     <section className="home">
@@ -20,7 +25,13 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+        {/* recent tx */}
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
