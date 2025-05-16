cd9931871d82c54aa525134cf46bdbc8dc3bd881
Musfiqur Sakib <m.r.sakib@outlook.com>
2025-05-16 16:59:29 +1000

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
             alt="auth-image"
             width={500}
             height={500}
+            priority
           />
         </div>
       </div>
